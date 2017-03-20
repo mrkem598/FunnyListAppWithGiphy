bbe337f45a45ff2f95f9c55deb290ecc83822ce1
Moh <mohakemal9@gmail.com>
2017-03-20 17:21:29 -0400

    updated_Commit

--- app.js
+++ app.js
@@ -1,49 +1,106 @@
-//creating an array of professionals of Giphy api
-var professionals = ["Teachers",'Nurses','Doctors','developers','plumbers','farmers','polices','drivers','enginers','bellmans','chefs','politicians','artists','lawyers','architect'];
-var prof; 
+// document ready function
+$(document).ready(function() {
+  //we will create a click function 
+  $("button").on("click", function() {
+    //Declare my veriables called professionals
+    var professionals = $(this).data("name");
+    //declaring the queryUrl
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + professionals + "&api_key=dc6zaTOxFJmzC&limit=10";
+    // // performing an a aJax get request to the queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    // After the data from the AJAX request comes back 
+    .done(function(response) {
 
-// Using a for loop to create buttons for each array of professionals listed above
-// Also adding a link features to each buttons
-function addLinks () {
-  for (var i = 0; i < professionals.length; i++) {
-    link = document.createElement("button");
-    link.textContent = professionals[i];
-    link.className = "professionals";
-    document.body.appendChild(link);
-    
-  };
-};
+      console.log(response);
 
-addLinks();
-// Creating a click function for each buttons 
-$(".professionals").click(function(){
-  // this keyword reffers to the button that was cliked
-	var prof = $(this).attr("professionals");
- /* var queryURL = this.value();*/
- /*console.log(this = prof);*/
-}); 
-// constructing URL to search for professionals
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + prof + "&api_key=dc6zaTOxFJmzC&limit=10";
+      var results = response.data;
 
-// performing an a aJax get request to the queryURL
-$.ajax({
-  url: queryURL,
-  method: "GET"
-})
-// After the data from the AJAX request comes back 
-.done(function(response) {
-  // 
- $("#a").text(JSON.stringify(response));
+      for(var i = 0; i < results.length; i++) {
+        var professionalsDiv = $("<div/>");
+        var p = $("<p/>");
+        p.text(results[i].rating);
+        var professionalsImage = $("<img/>");
+        professionalsImage.addClass("anImg");
+        professionalsImage.attr("src", results[i].images.fixed_height.url);
+        professionalsImage.attr("data-still", results[i].images.fixed_height_still.url)
+        professionalsImage.attr("data-animate", results[i].images.fixed_height.url)
+        .attr("data-state", "still");
+        professionalsDiv.append(p);
+        professionalsDiv.append(professionalsImage);
+        professionalsDiv.prependTo($("#prof"));
+      }
+      $(".anImg").on("click", function(){
+        var state = $(this).attr("data-state");
+        console.log(this);
+        if(state == "still") {
+          $(this).attr("src", $(this).data("animate"));
+          $(this).attr("data-state", "animate");
+        }else{
+          $(this).attr("src", $(this).data("still"));
+          $(this).attr("data-state", "still");
+        }
+      });
+
+    });
+
+  });
+
+  var professionals = [""];
+  // adding the buttons with a click function
+  $("#theButton").on("click", function() {
+    var professionalsButton = $("gif-input").val();
+    //adding the new professionals
+    var newButton = $("<button/>").addClass("btn btn-info professionals").attr("data-name", professionalsButton).html(professionalsButton);
+    $("#professionalsButton").append(newButton);
+    console.log("work");
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + professionalsButton + "&api_key=dc6zaTOxFJmzC&limit=10";
+    console.log(professionalsButton);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .done(function(response) {
+      var results = response.data;
+      for(var i = 0; i< results.length; i++) {
+        var professionalsDiv = $("<div/>");
+        var p = $("<p/>");
+        p.text(results[i].rating);
+        var professionalsImage = $("<img/>");
+        professionalsImage.addClass('anImg')
+        professionalsImage.attr('src', results[i].images.fixed_height_still.url);
+        professionalsImage.attr('data-still', results[i].images.fixed_height_still.url)
+        professionalsImage.attr('data-animate', results[i].images.fixed_height.url)
+        .attr('data-state', 'still');
+        professionalsDiv.append(p);
+        professionalsDiv.append(professionalsImage);
+        professionalsDiv.prependTo($('#gifs'));
+      }
+
+      $('.anImg').on('click', function() {
+
+        var state = $(this).attr('data-state'); 
+        console.log(this);
+
+        if (state == 'still') {
+
+          $(this).attr('src', $(this).data('animate'));
+
+          $(this).attr('data-state', 'animate');
+
+        } else {
+
+          $(this).attr('src', $(this).data('still'));
+
+          $(this).attr('data-state', 'still');
+        }      
+      });
+    });
+
+    $("#gif-input").val("");
+    return false;
+  })
+  
 });
- gifDiv.append("#a");
- 
-// Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-$("").append(gifDiv);
-
-// Adding stop and play function to each giphy
-
-
-
-
-
-
